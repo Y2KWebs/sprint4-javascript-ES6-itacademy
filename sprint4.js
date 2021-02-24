307087bb1ff4cffaab80dfb4d2ed791d5af52be0
Manuel Congosto <mc@y2kwebs.com>
2021-02-24 20:01:48 +0100

    Exercici 5

--- sprint4.js
+++ sprint4.js
@@ -1,3 +1,8 @@
-let str = "abcde";
-let reverse = (str) => [...str].reverse().reduce((a, c) => a + c);
-console.log(reverse);
+function b() {
+  // ...
+}
+async function a() {
+  await b().then(() => {
+    doMoreWork();
+  });
+}
