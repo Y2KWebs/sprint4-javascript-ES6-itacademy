cf996fb2d12f6b0c18017c7d2371da069b097405
Manuel Congosto <mc@y2kwebs.com>
2021-02-24 21:25:47 +0100

    Exercici 6

--- sprint4.js
+++ sprint4.js
@@ -1,8 +1,20 @@
-function b() {
-  // ...
-}
-async function a() {
-  await b().then(() => {
-    doMoreWork();
-  });
-}
+var tasks = [
+  {
+    name: "Start React web",
+    duration: 120,
+  },
+  {
+    name: "Work out",
+    duration: 60,
+  },
+  {
+    name: "Procrastinate on facebook",
+    duration: 240,
+  },
+];
+var res = [];
+tasks.forEach((e) => res.push(e.name));
+console.log(res);
+
+const res2 = tasks.map((x) => x.name);
+console.log(res2);
