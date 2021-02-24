a99754b1be953e898623c6941fd06588cb05ccf2
Manuel Congosto <mc@y2kwebs.com>
2021-02-24 16:59:00 +0100

    Exercici 2

--- sprint4.js
+++ sprint4.js
@@ -1,23 +1,9 @@
-let multiply = (num1, num2) => num1 * num2;
+console.log(users.map((user) => user.firstName)); //o asignarlos a variable
 
-let toCelsius = (fahrenheit) => (5 / 9) * (fahrenheit - 32);
-
-// This function returns a string padded with leading zeros
-let padZeros = (num, totalLen) => {
-  var numStr = num.toString();
-  var numZeros = totalLen - numStr.length;
-  for (var i = 1; i <= numZeros; i++) {
-    numStr = "0" + numStr;
-  }
-  return numStr;
-};
-
-let power = (base, exponent) => {
-  var result = 1;
-  for (var i = 0; i < exponent; i++) {
-    result *= base;
-  }
-  return result;
-};
-
-let greet = (who) => console.log("Hello " + who);
+var users = [
+  { firstName: "Homer", lastName: "Simpson" },
+  { firstName: "Marge", lastName: "Simpson" },
+  { firstName: "Bart", lastName: "Simpson" },
+  { firstName: "Lisa", lastName: "Simpson" },
+  { firstName: "Maggie", lastName: "Simpson" },
+];
